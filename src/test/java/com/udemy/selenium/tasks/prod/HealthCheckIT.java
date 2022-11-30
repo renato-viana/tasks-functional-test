21cedabdfe85f8bf70e4816468f927a5f3ea0b16
Renato Viana <renatoviana30@gmail.com>
2022-11-30 15:48:08 -0300

    Health Check

---
+++ src/test/java/com/udemy/selenium/tasks/prod/HealthCheckIT.java
@@ -0,0 +1,30 @@
+package com.udemy.selenium.tasks.prod;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class HealthCheckIT {
+
+    @Test
+    public void healthCheck() throws MalformedURLException {
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.100.64:4444/wd/hub"), cap);
+        try {
+            driver.navigate().to("http://192.168.100.64:9999/tasks");
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            String version = driver.findElement(By.id("version")).getText();
+            Assert.assertTrue(version.startsWith("build"));
+        } finally {
+            driver.quit();
+        }
+    }
+
+}
